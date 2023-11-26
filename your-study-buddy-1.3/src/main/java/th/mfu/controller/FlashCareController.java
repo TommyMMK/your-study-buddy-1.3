09e8b7f3e402bdf67eb07b7a3ceb3633b4914d0a
SaiSithuPhyo <122963117+SaiSithuPhyo@users.noreply.github.com>
2023-11-26 04:32:34 +0000

    changes

--- your-study-buddy-1.3/src/main/java/th/mfu/controller/FlashCareController.java
+++ your-study-buddy-1.3/src/main/java/th/mfu/controller/FlashCareController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import th.mfu.domain.FlashCard;
@@ -35,6 +36,33 @@ public class FlashCareController {
     @PostMapping("/flashcards")
     public String saveFlashCard(@ModelAttribute FlashCard newflashcard) {
         flashcardRepo.save(newflashcard);
+        return "redirect:/flashcards";
+    }
+
+    @GetMapping("/edit-flashcard/{id}")
+    public String editFlashCardForm(@PathVariable Long id, Model model) {
+        FlashCard flashcard = flashcardRepo.findById(id).orElse(null);
+        model.addAttribute("editflashcard", flashcard);
+        return "EditFlashCard";
+    }
+
+    // Update flashcard
+    @PostMapping("/update-flashcard/{id}")
+    public String updateFlashCard(@PathVariable Long id, @ModelAttribute FlashCard updatedFlashCard) {
+        FlashCard flashcard = flashcardRepo.findById(id).orElse(null);
+        if (flashcard != null) {
+            flashcard.setName(updatedFlashCard.getName());
+            flashcard.setFrontLabel(updatedFlashCard.getFrontLabel());
+            flashcard.setBackLabel(updatedFlashCard.getBackLabel());
+            flashcardRepo.save(flashcard);
+        }
+        return "redirect:/flashcards";
+    }
+
+    // Delete flashcard
+    @GetMapping("/delete-flashcard/{id}")
+    public String deleteFlashCard(@PathVariable Long id) {
+        flashcardRepo.deleteById(id);
         return "redirect:/flashcards";
     }
 
